22e655dab547d5c6c49417b4056be688613d4d71
Oxsadik <torikussadik404@gmail.com>
2026-08-16 18:42:36 +0600

    update and delete product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -17,3 +17,33 @@ export async function getProducts(req, res) {
     res.json(products);
 }
 
+export async function getProductsById(req, res) {
+    const product = await prisma.product.findUnique({
+        where: {id: Number(req.params.id)},
+    });
+    if (!product) return res.status(404).json({error: "Product not found"});
+    res.json(product);
+}
+
+
+export async function updateProduct(req, res) {
+    try {
+        const product = await prisma.product.update({
+            where: {id: Number(req.params.id)},
+            data: req.body,
+        });
+        res.json(product);
+    } catch(err) {
+        res.status(400).json({error: err.message})
+    }
+}
+
+export async function deleteProduct(req, res) {
+    try {
+        await prisma.product.delete({ where: {id: Number(req.params.id)}});
+        res.status(204).send();
+    } catch(err) {
+        res.status(400).json({error: err.message});
+    }
+}
+
