7d38b8d1dacd5e7d9beaa747eee39e3278b68cf1
Oxsadik <torikussadik404@gmail.com>
2026-08-16 17:54:36 +0600

    base setup done

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/test", (req, res) => res.json({ message: "it's alive !" }));
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({error: "kuch to garbar hain dayaa!!!"});
+});
+
+export default app;
