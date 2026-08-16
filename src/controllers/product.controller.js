961719a6012425839b1712911fd53a003467c46b
Oxsadik <torikussadik404@gmail.com>
2026-08-16 18:39:06 +0600

    create and get product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,1 +1,19 @@
 import prisma from '../prismaClient.js'; 
+
+export async function createProduct(req, res) {
+    try {
+        const {sku, name, quantityInStock} = req.body;
+        const product = await prisma.product.create({
+            data: { sku, name, quantityInStock: quantityInStock ?? 0},
+        });
+        res.status(201).json(product);
+    } catch (err) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+export async function getProducts(req, res) {
+    const products = await prisma.product.findMany();
+    res.json(products);
+}
+
